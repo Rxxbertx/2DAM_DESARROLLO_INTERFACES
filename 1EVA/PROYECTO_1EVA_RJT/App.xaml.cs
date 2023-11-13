f3af8e7cf09fa6c8589202ea8cc02c7562b09324
ROBERTO <robertojt6@gmail.com>
2023-11-13 20:51:05 +0100

    añadidos controles

--- 1EVA/PROYECTO_1EVA_RJT/App.xaml.cs
+++ 1EVA/PROYECTO_1EVA_RJT/App.xaml.cs
@@ -7,5 +7,10 @@ namespace PROYECTO_1EVA_RJT
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Error: " + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
