3c317847583bb6fbb3fc1fa114bb231adf869e1d
ROBERTO <robertojt6@gmail.com>
2023-11-15 21:45:13 +0100

    examen

---
+++ 1EVA/EjercicioPractico1EVA-RJT/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using System.Windows;
+
+namespace EjercicioPractico1EVA_RJT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //al hacer click en el boton calcular se ejecuta el codigo
+        private void calcular_Click(object sender, RoutedEventArgs e)
+        {
+
+            //calcular el tiempo en funcion de la distancia y el medio de transporte seleccionado
+            
+            decimal distanciaDecimal;
+            double tiempoDecimal;
+            
+
+            if (!decimal.TryParse(distancia.Text, out distanciaDecimal))
+                
+            {
+                MessageBox.Show("La distancia debe ser un número");
+                return;
+            }
+
+
+
+            distanciaDecimal = Decimal.Round(distanciaDecimal, 2);
+
+          
+
+            if (andando.IsChecked == true)
+            {
+
+                tiempoDecimal = (double)distanciaDecimal / constantes.VELOCIDAD_ANDANDO;
+
+            }
+            else if (patinete.IsChecked == true)
+            {
+                tiempoDecimal = (double)distanciaDecimal / constantes.VELOCIDAD_PATINETE;
+                
+            }
+            else if (bicicleta.IsChecked == true)
+            {
+                tiempoDecimal = (double)distanciaDecimal / constantes.VELOCIDAD_BICICLETA;
+               
+            }
+            else if (coche.IsChecked == true)
+            {
+                tiempoDecimal = (double)distanciaDecimal / constantes.VELOCIDAD_COCHE;
+                
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un medio de transporte");
+                return;
+
+            }
+
+
+
+            //aqui se muestra el tiempo en formato horas:minutos:segundos
+
+             TimeSpan ts = TimeSpan.FromHours(tiempoDecimal);
+            tiempoText.Text = string.Format("TIEMPO HORAS:MINUTOS:SEGUNDOS: {0}", new DateTime(ts.Ticks).ToString("HH:mm:ss"));
+
+
+
+
+        }
+
+    }
+}
---
+++ 1EVA/EjercicioPractico1EVA-RJT/constantes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPractico1EVA_RJT
+{
+    public static class constantes
+    {
+
+        //se declaran las constantes de velocidad
+
+        public static double VELOCIDAD_ANDANDO = 05;
+        public static double VELOCIDAD_PATINETE = 25;
+
+        public static double VELOCIDAD_BICICLETA = 20;
+        public static double VELOCIDAD_COCHE = 75;
+       
+
+    }
+}
