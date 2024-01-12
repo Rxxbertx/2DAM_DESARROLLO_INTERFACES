6946dafe76539769bb402d488a336ccab6fe23ca
r <robertojt6@gmail.com>
2024-01-12 15:20:48 +0100

    VIEWS: MAINWINDOW DONE 99% AND LOGIN WINDOW

--- 2EVA/PRACTICA_LOGIN_RJT/practicaLoginRJT/obj/Debug/net7.0-windows/InicioSesionUsuario_RJT.AssemblyInfo.cs
+++ 2EVA/PRACTICA_LOGIN_RJT/practicaLoginRJT/obj/Debug/net7.0-windows/InicioSesionUsuario_RJT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InicioSesionUsuario_RJT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92faa2dbdf86a4fa9fe471620960296d662eb835")]
 [assembly: System.Reflection.AssemblyProductAttribute("InicioSesionUsuario_RJT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InicioSesionUsuario_RJT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
